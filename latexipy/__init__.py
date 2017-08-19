30d71d2ecd26b6d30c1f34d7a8e27878bd6d70e7
Jean Nassar <jn.masasin+git@gmail.com>
2017-08-19 07:23:36 -0600

    Formatting.

--- latexipy/__init__.py
+++ latexipy/__init__.py
@@ -1,7 +1,5 @@
-# -*- coding: utf-8 -*-
+'''Top-level package for LaTeXiPy.'''
 
-"""Top-level package for LaTeXiPy."""
-
-__author__ = """Jean Nassar"""
+__author__ = '''Jean Nassar'''
 __email__ = 'jn.masasin@gmail.com'
 __version__ = '0.1.0'
